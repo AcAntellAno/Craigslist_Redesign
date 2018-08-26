a403d4eec9ba5113a7dea16df09273d5f6723fcc
AcAntellAno <acantellano1996@gmail.com>
2018-08-26 12:00:23 -0700

    Added index route

--- app.js
+++ app.js
@@ -3,7 +3,11 @@ var express = require('express'),
   mongoose = require('mongoose'),
   methodOverride = require('method-override'),
   sanitize = require('express-sanitizer'),
-  app = express();
+  Item = require('./models/Item.js'),
+  User = require('./models/User.js'),
+  Like = require('./models/Like.js')
+app = express();
+
 //const PORT = process.env.PORT;
 const PORT = 8080;
 const url = process.env.DATABASEURL || 'mongodb://localhost/craigslist';
@@ -16,6 +20,18 @@ app.get('/', (req, res) => {
   res.render('landing');
 });
 
+app.get('/items', (req, res) => {
+  //get all items form db
+  Item.find({}, (err, Items) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render('items', {
+        items: Item
+      });
+    }
+  });
+});
 
 app.listen(PORT, () => {
   console.log('Craigslist server is starting...');
