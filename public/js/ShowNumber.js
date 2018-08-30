62549adbd467a4c72685ac34baf4292adf9f99f8
AcAntellAno <acantellano1996@gmail.com>
2018-08-30 00:09:41 -0700

    Added contact information toggle

---
+++ public/js/ShowNumber.js
@@ -0,0 +1,8 @@
+var showNumber = () => {
+    var num = document.getElementById('number');
+    if (num.style.display === "none") {
+        num.style.display = "block";
+    } else {
+        num.style.display = "none";
+    }
+}
